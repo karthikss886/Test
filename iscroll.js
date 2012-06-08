ad9958338fa9db4a685be7c23548a0e315846d50
karthik <karthiks886@gmail.com>
2012-06-08 12:08:03 +0530

    s

--- iscroll.js
+++ iscroll.js
@@ -1,7 +1,7 @@
 /*!
  * iScroll v4.1.9 ~ Copyright (c) 2011 Matteo Spinelli, http://cubiq.org
  * Released under MIT license, http://cubiq.org/license
- karthik asdf asdf 
+ karthik asdf   
  */
 (function(){
 var m = Math, 
